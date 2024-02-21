105e533df02080c4cfee652909fc4886e2a66217
Cars-n <104526149+Cars-n@users.noreply.github.com>
2024-02-21 18:43:24 -0500

    added mongo folder

---
+++ topic-04-mongo/hello.py
@@ -0,0 +1,38 @@
+# flask allows for dynamically created webpages
+# run with flask --app hello run   
+from flask import Flask, jsonify, send_from_directory #jsonify is a lib from flask thay turns strutures into json
+app = Flask(__name__)
+
+@app.route('/') # this is called a decorator
+def get_index():
+    return  f"Hello, from the World!"
+
+@app.route('/hello')
+def get_hello():
+    me = "Carson"
+    return  f"Hello, {me} from the World!"
+
+@app.route('/goodbye')
+def get_goodbye():
+    me = "Carson"
+    return  f"Goodbye, {me} from the World!"
+
+@app.route("/data")
+def get_data():
+    data = [
+        {"name":"suzy","type":"dog"},
+        {"name":"sandy","type":"cat"}
+    ]
+    return jsonify(data)
+
+@app.route("/api/status")
+def get_status():
+    data = [
+        { "name":"suzy", "status":"sleeping"},
+        { "name":"dorothy", "status":"hungry"},
+    ]
+    return jsonify(data)
+
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory('.', path)
