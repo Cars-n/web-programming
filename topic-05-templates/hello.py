e072ea60d46726f77c1c031da664bd16ff038b69
Cars-n <104526149+Cars-n@users.noreply.github.com>
2024-03-04 17:48:38 -0500

    worked with mongita

---
+++ topic-05-templates/hello.py
@@ -0,0 +1,77 @@
+# flask allows for dynamically created webpages
+# run with flask --app hello run   
+from flask import Flask, jsonify, render_template #jsonify is a lib from flask thay turns strutures into json
+from mongita import MongitaClientDisk
+import json
+
+app = Flask(__name__)
+
+
+# Creating a Mongo database with classic science fiction movies information
+
+classic_sci_fi_movies = [
+    {"title": "Blade Runner", "year": 1982, "director": "Ridley Scott", "plot": "In a dystopian future, a blade runner must pursue and terminate four replicants who stole a ship in space and have returned to Earth to find their creator."},
+    {"title": "2001: A Space Odyssey", "year": 1968, "director": "Stanley Kubrick", "plot": "The film follows a voyage to Jupiter with the sentient computer HAL after the discovery of a mysterious monolith affecting human evolution."},
+    {"title": "Metropolis", "year": 1927, "director": "Fritz Lang", "plot": "In a futuristic city sharply divided between the working class and the city planners, the son of the city's mastermind falls in love with a working-class prophet who predicts the coming of a savior to mediate their differences."},
+    {"title": "The Day the Earth Stood Still", "year": 1951, "director": "Robert Wise", "plot": "An alien lands and tells the people of Earth that they must live peacefully or be destroyed as a danger to other planets."},
+    {"title": "Forbidden Planet", "year": 1956, "director": "Fred M. Wilcox", "plot": "A starship crew goes to investigate the silence of a planet's colony only to find two survivors and a deadly secret that one of them has."},
+    {"title": "A Clockwork Orange", "year": 1971, "director": "Stanley Kubrick", "plot": "In the future, a sadistic gang leader is imprisoned and volunteers for a conduct-aversion experiment, but it doesn't go as planned."},
+    {"title": "Solaris", "year": 1972, "director": "Andrei Tarkovsky", "plot": "A psychologist is sent to a station orbiting a distant planet in order to discover what has caused the crew to go insane."},
+    {"title": "Alien", "year": 1979, "director": "Ridley Scott", "plot": "The crew of a commercial spacecraft encounter a deadly lifeform after investigating an unknown transmission."},
+    {"title": "The War of the Worlds", "year": 1953, "director": "Byron Haskin", "plot": "Earth is invaded by Martians with unbeatable weapons and a cruel sense of humor."},
+    {"title": "The Time Machine", "year": 1960, "director": "George Pal", "plot": "A man's quest for knowledge turns into a desperate race through time after he invents a machine that can transport him through time."}
+]
+
+# Writing the data to a Mongo db file
+
+# create a mongita client connection
+client = MongitaClientDisk()
+
+# open a movie db
+movie_db = client.movie_db
+
+@app.route("/data/movies/scifi")
+def get_data_movies_scifi():
+    #with open("classic_sci_fi_movies.json","r") as f:
+    #    data = json.load(f)
+
+    # open a scifi collection
+    scifi_collection = movie_db.scifi_collection
+    data = list(scifi_collection.find({}))
+    for item in data:
+        del item["_id"] # removes mongo object id
+    return jsonify(data)
+
+@app.route("/hello")
+@app.route("/hello/<name>")
+def get_hello(name="guest"):
+    return render_template("hello.html", names=[name, "A", "B", "C"])
+
+@app.route("/movies")
+@app.route("/movies/<keyword>")
+def get_movies(keyword=None):
+    scifi_collection = movie_db.scifi_collection
+    data = list(scifi_collection.find({}))
+    for item in data:
+        del item["_id"] # removes mongo object id
+    if keyword:
+        data = [item for item in data if keyword in item['plot']]
+    return render_template("movies.html", movies=data)
+
+@app.route("/movies2")
+@app.route("/movies2/<keyword>")
+def get_movies2(keyword=None):
+    scifi_collection = movie_db.scifi_collection
+    if keyword:
+        data = list(scifi_collection.find({"title" : keyword}))
+    else:
+        data = list(scifi_collection.find({}))
+    for item in data:
+        del item["_id"] # removes mongo object id
+    if keyword:
+        data = [item for item in data if keyword in item['plot']]
+    return render_template("movies.html", movies=data)
+
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory('.', path)
